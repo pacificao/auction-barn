9b21ce90d421160903feb3f2eed37dfb1697a9e8
pacificao <nathan@pacificao.com>
2024-09-17 08:52:19 -0700

    Update git-pull-script.sh

--- git-pull-script.sh
+++ git-pull-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /mnt/volume_auction_barn/
-git pull origin main
+cd /mnt/volume_auction_barn/auction-barn
+git pull origin main >> /mnt/volume_auction_barn/auction-barn/git-pull.log 2>&1
