79c8dc3c06a83bb5edf1c3ec097034e6245f3ad0
pacificao <nathan@pacificao.com>
2024-09-17 20:24:06 -0700

    git fix and image resize

--- git-pull-script.sh
+++ git-pull-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /mnt/volume_auction_barn/auction-barn
-git pull origin main >> /mnt/volume_auction_barn/auction-barn/git-pull.log 2>&1
+git pull >> /mnt/volume_auction_barn/auction-barn/git-pull.log 2>&1
