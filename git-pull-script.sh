e69ba28bc577ca17818d926393b67f53e632a4f8
pacificao <nathan@pacificao.com>
2024-09-17 21:22:43 -0700

    Delete git-pull-script.sh

--- git-pull-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to the live site directory
-cd /mnt/volume_auction_barn/auction-barn
-
-# Perform git pull with rebase to avoid merge conflicts
-git pull --rebase >> /mnt/volume_auction_barn/auction-barn/git-pull.log 2>&1
