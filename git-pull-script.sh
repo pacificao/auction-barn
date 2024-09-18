1ba1a77cdc5a5b6d7498027878a19414d653535a
pacificao <nathan@pacificao.com>
2024-09-17 21:02:15 -0700

    Update git-pull-script.sh

--- git-pull-script.sh
+++ git-pull-script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Navigate to the live site directory
 cd /mnt/volume_auction_barn/auction-barn
-git pull >> /mnt/volume_auction_barn/auction-barn/git-pull.log 2>&1
+
+# Perform git pull with rebase to avoid merge conflicts
+git pull --rebase >> /mnt/volume_auction_barn/auction-barn/git-pull.log 2>&1
